06b8b8b9faafd381d1030ebd462cbdb7aa341a5b
somya-jangid <somyajangid78@gmail.com>
2025-02-24 00:54:48 +0530

    Add or update backend/routes/users.js

---
+++ backend/routes/users.js
@@ -0,0 +1,1 @@
+const express = require('express');const router = express.Router();const User = require('../models/User');router.post('/register', async (req, res) => { const user = new User(req.body); await user.save(); res.send(user); });module.exports = router;
